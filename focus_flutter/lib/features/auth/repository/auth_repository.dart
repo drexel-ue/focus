036df06d14682b9e86210c59b722f1b2f3699782
IKesh Pack <ikesh@devangels.london>
2024-12-26 17:40:10 -0500

    feat: persist session

--- focus_flutter/lib/features/auth/repository/auth_repository.dart
+++ focus_flutter/lib/features/auth/repository/auth_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:clerk_auth/clerk_auth.dart';
 import 'package:flutter/widgets.dart';
@@ -6,6 +8,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focus_client/focus_client.dart';
 import 'package:focus_flutter/api/api_client.dart';
 import 'package:focus_flutter/features/auth/repository/clerk_auth_provider.dart';
+import 'package:path_provider/path_provider.dart';
 
 /// Provides acccess to [AuthRepository].
 final authRepositoryProvider = AsyncNotifierProvider<AuthRepository, AuthSession>(() {
@@ -15,6 +18,8 @@ final authRepositoryProvider = AsyncNotifierProvider<AuthRepository, AuthSession
 /// Manages [AuthSession].
 class AuthRepository extends AsyncNotifier<AuthSession>
     with ClerkAuthProviderRef, Logging, ApiClientRef, ChangeNotifier {
+  late File _file;
+
   @override
   set state(AsyncValue<AuthSession> value) {
     super.state = value;
@@ -38,6 +43,7 @@ class AuthRepository extends AsyncNotifier<AuthSession>
         final authResponse = await api.auth.authenticate();
         await api.authenticationKeyManager!.put(authResponse.token!.accessToken);
         state = AsyncData(authResponse);
+        await _storeAuthSession();
       } else {
         state = AsyncData(AuthSession());
       }
@@ -50,9 +56,30 @@ class AuthRepository extends AsyncNotifier<AuthSession>
     }
   }
 
-  @override
-  FutureOr<AuthSession> build() {
-    // FIXME(drexel-ul): restore session.
-    return AuthSession();
+  Future<AuthSession> _maybeRestoreSession() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      _file = File('${directory.path}/auth_key_store.json');
+      if (await _file.exists()) {
+        final content = await _file.readAsString();
+        final jsonSerialization = json.decode(content);
+        return AuthSession.fromJson(jsonSerialization);
+      } else {
+        final jsonSerialization = json.encode(AuthSession().toJson());
+        await _file.writeAsString(jsonSerialization);
+        return AuthSession();
+      }
+    } catch (error, stackTrace) {
+      logSevere('error in _maybeRestoreSession', error, stackTrace);
+      return AuthSession();
+    }
   }
+
+  Future<void> _storeAuthSession() async {
+    final jsonSerialization = json.encode(state.requireValue);
+    await _file.writeAsString(jsonSerialization);
+  }
+
+  @override
+  FutureOr<AuthSession> build() async => _maybeRestoreSession();
 }
