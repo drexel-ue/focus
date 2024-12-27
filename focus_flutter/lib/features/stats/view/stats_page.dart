4d3e9ec54a7fac08bcdb616acc2a80cf67ae9ab0
IKesh Pack <ikesh@devangels.london>
2024-12-26 20:50:20 -0500

    feat: stats page header

--- focus_flutter/lib/features/stats/view/stats_page.dart
+++ focus_flutter/lib/features/stats/view/stats_page.dart
@@ -1,13 +1,58 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:focus_client/focus_client.dart';
+import 'package:focus_flutter/app/assets.dart';
+import 'package:focus_flutter/app/layout.dart';
+import 'package:focus_flutter/features/auth/repository/auth_repository.dart';
 
 /// Stats Page.
 @immutable
-class StatsPage extends StatelessWidget {
+class StatsPage extends ConsumerWidget {
   /// Construcs a const [StatsPage].
   const StatsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Stats'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(authRepositoryProvider).requireValue.user!;
+    return Padding(
+      padding: allPadding16,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Text(user.fullName),
+              ),
+              horizontalMargin16,
+              SizedBox.square(
+                dimension: 64.0,
+                child: Material(
+                  type: MaterialType.circle,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: allPadding2,
+                    child: Material(
+                      clipBehavior: Clip.antiAlias,
+                      type: MaterialType.circle,
+                      color: Colors.black,
+                      child: Image.network(
+                        user.profileImageUrl ?? '',
+                        errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                          return SvgPicture.asset(AppAssets.defaultAvatar);
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
