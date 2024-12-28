cf3bd1947e77241a14d97d07619f9b793729e0a7
IKesh Pack <ikesh@devangels.london>
2024-12-28 17:02:56 -0500

    feat: create task flow

--- focus_flutter/lib/features/tasks/repository/tasks_repository.dart
+++ focus_flutter/lib/features/tasks/repository/tasks_repository.dart
@@ -4,6 +4,7 @@ import 'package:clerk_flutter/logging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focus_client/focus_client.dart';
 import 'package:focus_flutter/api/api_client.dart';
+import 'package:focus_flutter/features/home/repository/home_repository.dart';
 import 'package:focus_flutter/features/tasks/repository/task_state.dart';
 
 export 'package:focus_flutter/features/tasks/repository/task_state.dart';
@@ -14,7 +15,7 @@ final taskRepositoryProvider = AsyncNotifierProvider<TasksRepository, TaskState>
 });
 
 /// Manages [Task]s.
-class TasksRepository extends AsyncNotifier<TaskState> with ApiClientRef, Logging {
+class TasksRepository extends AsyncNotifier<TaskState> with ApiClientRef, Logging, HomeRepoRef {
   /// Loads tasks into memory with pagination.
   Future<void> loadTasks() async {
     final currentState = state;
@@ -32,6 +33,7 @@ class TasksRepository extends AsyncNotifier<TaskState> with ApiClientRef, Loggin
     } catch (error, stackTrace) {
       logSevere('error in loadTasks', error, stackTrace);
       state = AsyncError<TaskState>(error, stackTrace).copyWithPrevious(currentState);
+      homeRepo.showNegativeSnack('Failed to load tasks');
     }
   }
 
@@ -44,6 +46,7 @@ class TasksRepository extends AsyncNotifier<TaskState> with ApiClientRef, Loggin
     final currentState = state;
     try {
       state = const AsyncLoading<TaskState>().copyWithPrevious(currentState);
+      homeRepo.showSnack('Creating task: $title...');
       final task = await refreshIfNeeded((api) async {
         return await api.task.createTask(
           title: title,
@@ -56,9 +59,11 @@ class TasksRepository extends AsyncNotifier<TaskState> with ApiClientRef, Loggin
           tasks: [task!, ...currentState.requireValue.tasks],
         ),
       );
+      homeRepo.showPositiveSnack('Created task: $title');
     } catch (error, stackTrace) {
       logSevere('error in createTask', error, stackTrace);
       state = AsyncError<TaskState>(error, stackTrace).copyWithPrevious(currentState);
+      homeRepo.showNegativeSnack('Failed to create task: $title');
     }
   }
 
@@ -74,6 +79,7 @@ class TasksRepository extends AsyncNotifier<TaskState> with ApiClientRef, Loggin
       );
     } catch (error, stackTrace) {
       logSevere('error in build', error, stackTrace);
+      homeRepo.showNegativeSnack('Failed to load tasks');
       return const TaskState();
     }
   }
--- focus_flutter/lib/features/tasks/view/create_task_form.dart
+++ focus_flutter/lib/features/tasks/view/create_task_form.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focus_client/focus_client.dart';
 import 'package:focus_flutter/app/layout.dart';
+import 'package:focus_flutter/features/tasks/repository/tasks_repository.dart';
 import 'package:focus_flutter/features/widget/digits_only_input_formatter.dart';
 import 'package:focus_flutter/features/widget/focus_button.dart';
 
@@ -37,6 +38,22 @@ class _CreateTaskFormState extends ConsumerState<CreateTaskForm> {
     return true;
   }
 
+  Future<void> _createTask() async {
+    await ref.read(taskRepositoryProvider.notifier).createTask(
+          title: _titleController.text.trim(),
+          description: _descriptionController.text.trim(),
+          abilityExpValues: _abilityExpValues.entries.map<AbilityExperienceValue>((entry) {
+            return AbilityExperienceValue(
+              ability: entry.key,
+              exp: int.parse(entry.value.text),
+            );
+          }).toList(),
+        );
+    if (mounted) {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   void dispose() {
     _titleController
@@ -55,85 +72,96 @@ class _CreateTaskFormState extends ConsumerState<CreateTaskForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: allPadding16,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextField(
-            controller: _titleController,
-            cursorColor: Colors.white,
-            decoration: const InputDecoration(
-              hintText: 'Title',
-            ),
-          ),
-          verticalMargin16,
-          GridView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200.0,
-              mainAxisExtent: 48.0,
-              mainAxisSpacing: 16.0,
-              crossAxisSpacing: 16.0,
-            ),
-            itemCount: Ability.values.length,
-            itemBuilder: (BuildContext context, int index) {
-              final ability = Ability.values[index];
-              return Row(
+    return Stack(
+      children: [
+        Padding(
+          padding: allPadding16,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _titleController,
+                cursorColor: Colors.white,
+                decoration: const InputDecoration(
+                  hintText: 'Title',
+                ),
+              ),
+              verticalMargin16,
+              GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200.0,
+                  mainAxisExtent: 48.0,
+                  mainAxisSpacing: 16.0,
+                  crossAxisSpacing: 16.0,
+                ),
+                itemCount: Ability.values.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ability = Ability.values[index];
+                  return Row(
+                    children: [
+                      Expanded(child: Text(ability.name.substring(0, 3).toUpperCase())),
+                      horizontalMargin8,
+                      Expanded(
+                        child: TextField(
+                          controller: _abilityExpValues[ability],
+                          textAlign: TextAlign.center,
+                          textAlignVertical: TextAlignVertical.center,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            DigitOnlyInputFormatter(),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              verticalMargin16,
+              Expanded(
+                child: TextField(
+                  controller: _descriptionController,
+                  expands: true,
+                  maxLines: null,
+                  textAlignVertical: TextAlignVertical.top,
+                  decoration: const InputDecoration(
+                    hintText: 'Description',
+                  ),
+                ),
+              ),
+              verticalMargin16,
+              Row(
                 children: [
-                  Expanded(child: Text(ability.name.substring(0, 3).toUpperCase())),
-                  horizontalMargin8,
                   Expanded(
-                    child: TextField(
-                      controller: _abilityExpValues[ability],
-                      textAlign: TextAlign.center,
-                      textAlignVertical: TextAlignVertical.center,
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [
-                        DigitOnlyInputFormatter(),
-                        FilteringTextInputFormatter.digitsOnly,
-                      ],
+                    child: FocusButton(
+                      onTap: () => Navigator.of(context).pop(),
+                      child: const Text('Cancel'),
+                    ),
+                  ),
+                  horizontalMargin16,
+                  Expanded(
+                    child: FocusButton(
+                      onTap: () => _createTask(),
+                      filled: true,
+                      enabled: _createEnabled,
+                      child: const Text('Create'),
                     ),
                   ),
                 ],
-              );
-            },
-          ),
-          verticalMargin16,
-          Expanded(
-            child: TextField(
-              controller: _descriptionController,
-              expands: true,
-              maxLines: null,
-              textAlignVertical: TextAlignVertical.top,
-              decoration: const InputDecoration(
-                hintText: 'Description',
-              ),
-            ),
-          ),
-          verticalMargin16,
-          Row(
-            children: [
-              Expanded(
-                child: FocusButton(
-                  onTap: () => Navigator.of(context).pop(),
-                  child: const Text('Cancel'),
-                ),
-              ),
-              horizontalMargin16,
-              Expanded(
-                child: FocusButton(
-                  onTap: () {},
-                  filled: true,
-                  enabled: _createEnabled,
-                  child: const Text('Create'),
-                ),
               ),
             ],
           ),
-        ],
-      ),
+        ),
+        if (ref.watch(taskRepositoryProvider).isLoading) //
+          const Material(
+            color: Colors.white54,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+      ],
     );
   }
 }
