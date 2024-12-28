d8cea2c85f52394bd668763acbadbcca8577ef2e
IKesh Pack <ikesh@devangels.london>
2024-12-28 03:55:56 -0500

    feat: empty tasks ui

--- focus_flutter/lib/features/tasks/view/tasks_page.dart
+++ focus_flutter/lib/features/tasks/view/tasks_page.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:focus_client/focus_client.dart';
 import 'package:focus_flutter/app/layout.dart';
+import 'package:focus_flutter/features/tasks/repository/tasks_repository.dart';
+import 'package:focus_flutter/features/widget/focus_button.dart';
 
 /// Tasks Page.
 @immutable
-class TasksPage extends StatelessWidget {
+class TasksPage extends ConsumerWidget {
   /// Construcs a const [TasksPage].
   const TasksPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tasks = ref.watch(taskRepositoryProvider).value?.tasks ?? const <Task>[];
+    if (tasks.isEmpty) {
+      return Center(
+        child: SizedBox(
+          width: 200.0,
+          child: FocusButton(
+            onTap: () {},
+            child: const Text('Create task'),
+          ),
+        ),
+      );
+    }
+    return const Padding(
       padding: allPadding16,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
