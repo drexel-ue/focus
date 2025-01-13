a64ac51485b050c3feb3facf746f803329f4c63e
IKesh Pack <ikesh@devangels.london>
2025-01-12 19:19:06 -0500

    feat: add scroll shadow to task page

--- focus_flutter/lib/features/tasks/view/tasks_page.dart
+++ focus_flutter/lib/features/tasks/view/tasks_page.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
@@ -10,6 +9,7 @@ import 'package:focus_flutter/features/tasks/view/task_form.dart';
 import 'package:focus_flutter/features/widgets/focus_button.dart';
 import 'package:focus_flutter/features/widgets/focus_checkbox.dart';
 import 'package:focus_flutter/features/widgets/focus_modal.dart';
+import 'package:focus_flutter/features/widgets/scroll_shadow.dart';
 
 /// Tasks Page.
 @immutable
@@ -63,112 +63,80 @@ class _TasksPageState extends ConsumerState<TasksPage> {
         ),
       );
     }
-    final stat = tasks.fold(
-      UserAbilityStats(
-        strengthExp: 0,
-        vitalityExp: 0,
-        agilityExp: 0,
-        intelligenceExp: 0,
-        perceptionExp: 0,
-      ),
-      (UserAbilityStats folded, Task task) {
-        if (task.completed) {
-          return folded;
-        }
-        return folded + task.abilityExpValues;
-      },
-    );
-    return Padding(
-      padding: horizontalPadding16 + topPadding2,
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            actions: [
-              IconButton(
-                onPressed: () => _showTaskForm(context),
-                icon: const Icon(Icons.add, color: Colors.white),
-              ),
-            ],
-            expandedHeight: 160.0,
-            backgroundColor: Colors.black,
-            flexibleSpace: FlexibleSpaceBar(
-              stretchModes: const [StretchMode.fadeTitle],
-              background: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  verticalMargin16,
-                  Text('Complete: ${tasks.where((task) => task.completed).length}'),
-                  Text('Incomplete: ${tasks.whereNot((task) => task.completed).length}'),
-                  Text('STR avail: ${stat.strengthExp}'),
-                  Text('VIT avail: ${stat.vitalityExp}'),
-                  Text('AGI avail: ${stat.agilityExp}'),
-                  Text('INT avail: ${stat.intelligenceExp}'),
-                  Text('PER avail: ${stat.perceptionExp}'),
-                ],
-              ),
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
+          child: IconButton(
+            onPressed: () => _showTaskForm(context),
+            icon: const Icon(Icons.add, color: Colors.white),
           ),
-          SliverList.builder(
-            itemCount: tasks.length,
-            itemBuilder: (BuildContext context, int index) {
-              final task = tasks[index];
-              return Padding(
-                padding: bottomPadding16,
-                child: Slidable(
-                  endActionPane: ActionPane(
-                    motion: const BehindMotion(),
-                    children: [
-                      SlidableAction(
-                        onPressed: (_) => _showDeleteModal(context, task),
-                        icon: Icons.delete_forever_sharp,
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                  child: InkWell(
-                    splashColor: Colors.transparent,
-                    onTap: () => _showTaskForm(context, task),
-                    child: Row(
+        ),
+        Expanded(
+          child: ScrollShadow(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                final task = tasks[index];
+                return Padding(
+                  padding: bottomPadding16,
+                  child: Slidable(
+                    endActionPane: ActionPane(
+                      motion: const BehindMotion(),
                       children: [
-                        FocusCheckbox(
-                          onTap: () => notifier.toggleTaskComplete(task.id!),
-                          selected: task.completed,
-                        ),
-                        horizontalMargin16,
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                task.title,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextTheme.of(context).titleMedium,
-                              ),
-                              if (task.description case String description) ...[
-                                // verticalMargin4,
-                                Text(
-                                  description,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ],
-                            ],
-                          ),
+                        SlidableAction(
+                          onPressed: (_) => _showDeleteModal(context, task),
+                          icon: Icons.delete_forever_sharp,
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
                         ),
                       ],
                     ),
+                    child: InkWell(
+                      splashColor: Colors.transparent,
+                      onTap: () => _showTaskForm(context, task),
+                      child: Row(
+                        children: [
+                          FocusCheckbox(
+                            onTap: () => notifier.toggleTaskComplete(task.id!),
+                            selected: task.completed,
+                          ),
+                          horizontalMargin16,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  task.title,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextTheme.of(context).titleMedium,
+                                ),
+                                if (task.description case String description) ...[
+                                  // verticalMargin4,
+                                  Text(
+                                    description,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
