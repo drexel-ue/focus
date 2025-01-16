1f0784fa1ca571a588012d66db66802bb7329d80
IKesh Pack <ikesh@devangels.london>
2025-01-16 07:28:45 -0500

    feat: portrait only

--- focus_flutter/lib/main.dart
+++ focus_flutter/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focus_flutter/app/app.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const ProviderScope(child: FocusApp()));
 }
